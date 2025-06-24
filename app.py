9dc2be2e4696e26f470b3de097591cc3cf561133
juyoung113 <mmhtn@naver.com>
2025-06-24 10:01:38 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+
+#import mod1
+#from mod1 import *
+#import mod1 as mo
+import mod.mod1 as mod
+
+
+
+print(mod.PI)
+print(mod.add(100,20))
+print(mod.sub(10,5))
+
+# print(mo.PI)
+# print(mo.add(100,20))
+# print(mo.sub(10,5))
+
+#print(mod1.PI)
+#print(mod1.add(10,20))
+#print(mod1.sub(10,2))
